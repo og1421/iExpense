8c6a37a45be26adfe2aadf9788bacdb278a2f293
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 17:56:43 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
